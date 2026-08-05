28c38e46ab94151c319a1535f43cf00fec1995e5
kshitijanand6205 <shivamsinghrajput9570@gmail.com>
2026-08-06 00:38:08 +0530

    Add a functionality setActivateButton for better ux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './style.css';
 import loadHome from './pages/home.js';
+import loadContact from './pages/contact.js';
 import loadMenu from './pages/menu.js';
 
 const homeBtn = document.querySelector('#home-btn');
 const menuBtn = document.querySelector('#menu-btn');
+const contactBtn = document.querySelector('#contact-btn');
 
 loadHome();
 
@@ -12,14 +14,31 @@ function clearContent() {
     content.replaceChildren();
 }
 
+function setActiveButton(button) {
+    const buttons = document.querySelectorAll('nav button');
+    buttons.forEach((btn) => {
+        btn.classList.remove('active');
+    });
+    button.classList.add('active');
+}
+
 homeBtn.addEventListener('click', () => {
     clearContent();
     loadHome();
+    setActiveButton(homeBtn);
 });
 
 menuBtn.addEventListener('click', () => {
     clearContent();
     loadMenu();
+    setActiveButton(menuBtn);
 });
 
+contactBtn.addEventListener('click', () => {
+    clearContent();
+    loadContact();
+    setActiveButton(contactBtn);
+});
+
+export {setActiveButton, clearContent};
 
