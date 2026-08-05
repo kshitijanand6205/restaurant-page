4f1a2636f1fc22a139d579302a45df8fec076e0a
kshitijanand6205 <shivamsinghrajput9570@gmail.com>
2026-08-05 17:21:41 +0530

    Add restaurant picture on the page and fix some function components

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,25 @@
+import restaurantImage from '../assets/restaurant.png';
+
 function loadHome() {
     const content = document.querySelector('#content');
 
     const home = document.createElement('div');
+    home.classList.add('home');
     
     const heading = document.createElement('h1');
-
     heading.textContent = 'Kshitij Restaurant';
 
+    const description = document.createElement('p');
+    description.textContent = 
+    'Serving delicious food made with fresh ingredients everyday.';
+
+    const image = document.createElement('img');
+    image.src = restaurantImage;
+    image.alt = 'Restaurant';
+   
     home.appendChild(heading);
-    
+    home.appendChild(description);
+    home.appendChild(image);
     content.appendChild(home);
 };
 
