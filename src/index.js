46e82d838f7c020a3447d93e7e4d474f8916b4c3
kshitijanand6205 <shivamsinghrajput9570@gmail.com>
2026-08-05 17:23:47 +0530

    Initiated home page to default opening page and add menu button to navigate to menu page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import './style.css';
 import loadHome from './pages/home.js';
+import loadMenu from './pages/menu.js';
+
+const homeBtn = document.querySelector('#home-btn');
+const menuBtn = document.querySelector('#menu-btn');
 
 loadHome();
+
+function clearContent() {
+    const content = document.querySelector('#content');
+    content.replaceChildren();
+}
+
+homeBtn.addEventListener('click', () => {
+    clearContent();
+    loadHome();
+});
+
+menuBtn.addEventListener('click', () => {
+    clearContent();
+    loadMenu();
+});
+
+
