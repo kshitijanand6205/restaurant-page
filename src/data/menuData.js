11c5097bc6b80af7d70adcf5ae3d77a5756b3092
kshitijanand6205 <shivamsinghrajput9570@gmail.com>
2026-08-06 00:31:26 +0530

    Add searate menu data file for better architecture design

---
+++ src/data/menuData.js
@@ -0,0 +1,24 @@
+const menuItems = [
+    {
+        name: 'Margherita Pizza',
+        description: 'Fresh mozzarella, basil and tomato sauce.',
+        price: '$5',
+    },
+    {
+        name: 'Cheese Burger',
+        description: 'Juicy grilled beef with cheddar cheese.',
+        price: '$4',
+    },
+    {
+        name: 'Alfredo Pasta',
+        description: 'Creamy white sauce pasta.',
+        price: '$3',
+    },
+    {
+        name: 'Cold Coffee',
+        description: 'Freshly brewed coffee with ice cream.',
+        price: '$2',
+    },
+];
+
+export default menuItems;
